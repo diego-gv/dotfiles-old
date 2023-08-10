bc33dc13e39f354fc8d58cb5237be21f8e74cc5c
Diego S. García <diegosalvador.gv@gmail.com>
2023-08-10 19:16:07 +0200

    Uncomment apt installation

--- scripts/system.sh
+++ scripts/system.sh
@@ -13,27 +13,27 @@ FONTS_DIR="$HOME/.local/share/fonts"
 USER_BINARIES="$HOME/bin"
 
 do_install() {
-	# local packages=(
-	# 	build-essential
-	# 	cmake
-	# 	curl
-	# 	fontconfig
-	# 	alacritty
-	# 	git
-	# 	htop
-	# 	vim
-	# 	wget
-	# 	zsh
-	# )
+	local packages=(
+		build-essential
+		cmake
+		curl
+		fontconfig
+		alacritty
+		git
+		htop
+		vim
+		wget
+		zsh
+	)
 
-	# info "[system] Install packages"
-	# info "[system][apt] Add ppa repositories"
-	# export DEBIAN_FRONTEND=noninteractive
-	# sudo apt-add-repository -y ppa:git-core/ppa	# git repository
-	# sudo add-apt-repository -y ppa:aslatter/ppa # alacritty repository
-	# sudo apt-get update -qq
-	# info "[system][apt] Install apt packages"
-	# sudo apt-get install -qq -y "${packages[@]}"
+	info "[system] Install packages"
+	info "[system][apt] Add ppa repositories"
+	export DEBIAN_FRONTEND=noninteractive
+	sudo apt-add-repository -y ppa:git-core/ppa	# git repository
+	sudo add-apt-repository -y ppa:aslatter/ppa # alacritty repository
+	sudo apt-get update -qq
+	info "[system][apt] Install apt packages"
+	sudo apt-get install -qq -y "${packages[@]}"
 
 	info "[system][snap] Install snap packages"
 	sudo snap install --classic code

3ac379a7ebedda2eca5887a354ec91fe6dbde4b3
diego-gv <diegosalvador.gv@gmail.com>
2024-02-13 08:56:25 +0100

    new make command to tests: bash

--- test.mk
+++ test.mk
@@ -7,7 +7,9 @@ endif
 ##@ Testing makefile
 test: build ## Build and run tests
 	docker run -t --rm dotfiles/test make
-interactive: build ## Build, run tests and entry bash
+bash: build ## Build and entry bash
+	docker run -it --rm -v ${PWD}:/home/test/dotfiles dotfiles/test /bin/bash
+interactive: build ## Build, run tests and entry zsh
 	docker run -it --rm -v ${PWD}:/home/test/dotfiles dotfiles/test bash -c "make ; zsh"
 build: ## └ Build a docker image
 	docker build -t dotfiles/test --build-arg UBUNTU_VERSION=$(UBUNTU_VERSION) .
